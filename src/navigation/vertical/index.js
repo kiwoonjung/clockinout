b61ccea82c5d4fe0c89c93554277a597250ef1b5
kiwoonjung <kiwoon0627@gmail.com>
2024-03-11 00:56:20 -0700

    20240311 00:54 implemented calendar

--- src/navigation/vertical/index.js
+++ src/navigation/vertical/index.js
@@ -1,12 +1,17 @@
 export default [
   {
-    title: 'Home',
-    to: { name: 'index' },
-    icon: { icon: 'tabler-smart-home' },
+    title: "Home",
+    to: { name: "index" },
+    icon: { icon: "tabler-smart-home" },
   },
   {
-    title: 'Second page',
-    to: { name: 'second-page' },
-    icon: { icon: 'tabler-file' },
+    title: "Second page",
+    to: { name: "second-page" },
+    icon: { icon: "tabler-file" },
   },
-]
+  {
+    title: "Calendar",
+    to: { name: "calendar" },
+    icon: { icon: "tabler-smart-home" },
+  },
+];
